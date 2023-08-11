61537bc59f45751f17c4c1af7c130c4057a5d572
Xavier-Julian-Rodriguez <xavierjulianrod@gmail.com>
2023-08-11 15:21:23 -0400

    add Person object & PersonList array component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import './App.css';
 import { Greet } from './components/Greet'
+import { Person } from './components/Person'
+import { PersonList } from './components/PersonList';
 
 function App() {
+  const personName = {
+    first: 'Bruce',
+    last: 'Wayne',
+  }
+  const nameList = [
+    {
+      first: 'Peter',
+      last: 'Parker'
+    },
+    {
+      first: 'Inspector',
+      last: 'Gadget'
+    },
+    {
+      first: 'Optimus',
+      last: 'Prime'
+    },
+  ]
   return (
     <div className="App">
-     <Greet name='Xavier'/>
+     <Greet name='Xavier' messageCount={10} isLoggedIn={false}/>
+     <Person name={personName} />
+     <PersonList names={nameList} />
     </div>
   );
 }
